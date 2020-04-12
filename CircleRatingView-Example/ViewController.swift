95de8bd4240fad8514e34013083668f49569cac5
kishanr <kishanr@hyperlinkinfosystem.net.in>
2020-04-12 15:19:48 +0530

    Fix bug

---
+++ CircleRatingView-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircleRatingView-Example
+//
+//  Created by KISHAN_RAJA on 12/04/20.
+//  Copyright © 2020 HYPERLINK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
